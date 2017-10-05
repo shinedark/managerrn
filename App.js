21ab35760f373ea89bf17115aa25ae27beec00d7
shinedark <mide0303@gmail.com>
2017-10-05 11:02:01 -0500

    login

---
+++ App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
+import firebase from 'firebase';
+import reducers from './src/reducers';
+import LoginForm from './src/components/LoginForm';
+
+export default class App extends React.Component {
+  
+  componentWillMount(){
+    const config = {
+        apiKey: "AIzaSyAl8Wipi8ErYIczP7NKA-Iiw3Ru3MrJgAQ",
+        authDomain: "manager-8fdf6.firebaseapp.com",
+        databaseURL: "https://manager-8fdf6.firebaseio.com",
+        projectId: "manager-8fdf6",
+        storageBucket: "",
+        messagingSenderId: "1075715641630"
+      };
+      firebase.initializeApp(config);
+  }
+
+  render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+    return (
+      <Provider store = {store} >
+        <LoginForm/>
+      </Provider>
+    );
+  }
+}
+
+
