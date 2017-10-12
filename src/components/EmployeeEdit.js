920bd87bc2e70ac28e95d37d56e1398d220545b7
shinedark <mide0303@gmail.com>
2017-10-12 12:14:48 -0500

    hf

---
+++ src/components/EmployeeEdit.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import EmployeeForm from './EmployeeForm';
+import { Card, CardSection, Button } from ' ./common';
+
+class EmployeeEdit extends Component {
+	render(){
+		return (
+			<Card>
+				<CardSection>
+					<Button>
+						Save Changes
+					</Button>
+				</CardSection>
+			</Card>
+		);
+	}
+}
+
+export default EmployeeEdit;
